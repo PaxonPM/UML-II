0eea55d7fd38132c612feb9ad6c7e7040ed87491
Paw Møller <pawmoeller94@gmail.com>
2025-11-05 12:57:56 +0100

    Add Icrud interface for CRUD operations
    Introduces the Icrud generic interface in the PizzaStore namespace to define Create, Read, Update, and Delete methods for generic types. This provides a contract for implementing CRUD functionality in the application.

---
+++ PizzaStore/Icrud.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaStore
+{
+    interface Icrud<GenKey, GenObject>
+    {
+
+        void Create(GenObject Obj);
+        void Read(IdKey value);
+        void Update(StoringObject Obj);
+        void Delete(IdKey value);
+
+    }
+}
