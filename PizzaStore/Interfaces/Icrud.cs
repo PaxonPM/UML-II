5430d4a9b8462574f7436e41dc62da2622be97dc
Paw Møller <pawmoeller94@gmail.com>
2025-11-06 21:33:35 +0100

    Move and update Icrud interface
    Relocated Icrud.cs to the Interfaces folder and updated the interface to accept three generic parameters instead of two. Method signatures for Read and Delete now use different generic types.

---
+++ PizzaStore/Interfaces/Icrud.cs
@@ -0,0 +1,14 @@
+﻿
+namespace PizzaStore.Interfaces
+{
+    interface Icrud<Tone, Ttwo, Tobject>
+    {
+
+        void Create(Tobject Obj);
+        void Read(Tone one);
+        void Update(Tobject Obj);
+        void Delete(Ttwo two);
+        
+
+    }
+}
