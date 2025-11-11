e600d7f7e66216725fddcbf71f4921773ecb2c40
Paw Møller <pawmoeller94@gmail.com>
2025-11-11 18:20:51 +0100

    Add menu option enums for PizzaStore (#3)
    Introduces enums for main, order, sub-order, customer, and admin menus to centralize menu option definitions and improve code maintainability.

---
+++ PizzaStore/Utils/MenuEnums.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace PizzaStore.Utils
+{
+    enum MainMenuOptions
+    {
+        Exit = 0,
+        OrderMenu = 1,
+        CustomerMenu = 2,
+        AdminMenu = 3,
+        StoreTutorial = 4
+    }
+
+    enum OrderMenuOptions
+    {
+        Back = 0,
+        Order = 1,
+        PastOrders = 2,
+    }
+
+    enum SubOrderMenuOptions
+    {
+        Back = 0,
+        ViewMenu = 1,
+        AddPizza = 2,
+        PrintCurrentOrder = 3,
+        DelPizzaFromOrder = 4,
+        FinalizeOrder = 5
+    }
+    enum CustomerMenuOptions
+    {
+        Back = 0,
+        PrintCustomerFile = 1,
+        CreateCustomer = 2,
+        ReadCustomer = 3,
+        UpdateCustomer = 4,
+        DeleteCustomer = 5
+    }
+    enum AdminMenuOptions
+    {
+        Back = 0,
+        PrintMenu = 1,
+        CreatePizza = 2,
+        ReadPizza = 3,
+        UpdatePizza = 4,
+        DeletePizza = 5
+    }
+
+    
+}
